63fedd1441e517b187f4fac86fe8df0b80dfb9d3
TSUHA0 <1355738338@qq.com>
2021-04-14 15:42:40 +0800

    初始化登陆界面、管理界面

---
+++ web/admin/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        登陆页面
+        <a-button type="primary">登陆</a-button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
